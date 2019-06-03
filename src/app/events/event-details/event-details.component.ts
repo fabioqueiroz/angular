95c1f2b5d15dd719c1f6a77149e62eb7165010ea
Fabio <fabioqueiroz@survey-me.com>
2019-06-03 11:59:18 +0100

    p6 route params

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
     templateUrl: './event-details.component.html',
     styles:[
         `
         .container { padding-left: 20px; padding-right: 20px; }
-        .event-image: { height: 100px; }
+        .event-image { height: 100px; }
 
         `
     ]
@@ -14,13 +16,15 @@ import { EventService } from '../shared/event.service';
 export class EventDetailsComponent implements OnInit 
 {
     event:any
-    constructor(private eventService: EventService) 
+    constructor(private eventService: EventService, private route:ActivatedRoute) 
     {
 
     }
 
     ngOnInit(): void 
     {
-       this.event = this.eventService.getSingleEvent(1);
+       this.event = this.eventService.getSingleEvent(
+          + this.route.snapshot.params['id']
+       );
     }
 }
