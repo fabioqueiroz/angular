6074ab86785608f1c29e7d5c1bbf506a584e7069
Fabio <fabioqueiroz@survey-me.com>
2019-06-04 11:37:28 +0100

    p6 active link

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -8,6 +8,7 @@ import {Component} from '@angular/core';
         .nav.navbar-nav {font-size: 15 px;}
         #searchForm {margin-right: 100px;}
         @media (max-width: 1000px) {#searchForm {display:none}}
+        li > a.active-link {color: #D86635}
     `]
 })
 
